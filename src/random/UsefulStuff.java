a46e80011f8ff11d6d06d5202aa55a18c2be8593
upopple <deadmantennis@gmail.com>
2011-02-17 14:12:34 -0500

    Triangle numbers and UsefulStuff

--- src/random/UsefulStuff.java
+++ src/random/UsefulStuff.java
@@ -20,4 +20,37 @@ public class UsefulStuff {
 		
 	}
 	
+	public static int nextPrimeInSieve(boolean[] notPrimeNumber, int n, int m){
+		int i = n;
+		for(;i < m && notPrimeNumber[i]; i++);
+		if(i>=m) return -1;
+		else return i;
+	}
+	
+	static int nGoesIntoM(int n, int m){
+		if(m % n == 0) return 1 + nGoesIntoM(n, m/n);
+		else return 0;
+	}
+	
+	public static int numberOfFactors(int n){
+		boolean[] notPrimeNumber = EratosthenesSieve(n+1);
+		int factorCount = 1, currentFactorCount = 0;
+		
+		for(int i=2; i <= n; i++){
+			if(!notPrimeNumber[i]){
+				currentFactorCount = nGoesIntoM(i,n);
+				if(currentFactorCount != 0){
+					factorCount *= currentFactorCount + 1;
+					n /= Math.pow(i,currentFactorCount);
+				}
+			}
+		}
+		
+		return factorCount;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(numberOfFactors(105));
+	}
+	
 }
--- src/random/triangleNumbers.java
+++ src/random/triangleNumbers.java
@@ -1,6 +1,20 @@
 package random;
-import 
+
+import random.UsefulStuff;
 
 public class triangleNumbers {
-	
+	public static void main(String[] args){
+		int currentNaturalNumber = 8, currentSum = 28, currentDivisors = 0;
+		
+		while(currentDivisors < 500){
+			currentSum += currentNaturalNumber;
+			if(currentNaturalNumber % 2 == 0)
+				currentDivisors = UsefulStuff.numberOfFactors(currentNaturalNumber/2) * UsefulStuff.numberOfFactors((++currentNaturalNumber));
+			else
+				currentDivisors = UsefulStuff.numberOfFactors(currentNaturalNumber) * UsefulStuff.numberOfFactors((++currentNaturalNumber)/2);
+			System.out.println(currentNaturalNumber + " " + currentSum +" " + currentDivisors);
+		}
+		
+		System.out.println(currentSum);
+	}
 }
